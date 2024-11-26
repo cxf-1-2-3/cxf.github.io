5351d62e6e68ac3094a3835bcc6454504dd1ac82
chenxuefeng <2739294436@qq.com>
2024-11-26 18:36:01 +0800

    初始化

--- .vuepress/navbar.ts
+++
@@ -1,8 +0,0 @@
-import {NavItem} from "vuepress/config";
-
-export default [
-    {
-        text: "学习路线",
-        link: '/学习路线/'
-    },
-] as NavItem[];
---
+++ docs/.vuepress/components/javascriptCom.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id=""></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+let obj = { a: 1, b: 2 };
+function shallowClone(source) {
+  let target = {};
+  for (let i in source) {
+    if (source.hasOwnProperty(i)) {
+      target[i] = source[i];
+    }
+  }
+  return target;
+}
+let shallowCloneObj = shallowClone(obj);
+console.log(shallowCloneObj);
+</script>
+
+<style lang="scss" scoped></style>
